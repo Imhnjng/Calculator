24edc58372911bfcdd8b37e2292d459c0a344da0
HyeonJeong <dlaus1214@gmail.com>
2024-03-19 18:00:02 +0900

    baseballGame 3-6

--- baseballGame/baseballGame/BaseballGame.swift
+++ baseballGame/baseballGame/BaseballGame.swift
@@ -7,116 +7,164 @@
 
 import Foundation
 
-class BaseballGame {
-    var title: String?
+class BaseballGame {                                                //재사용을 위한 class 만들기
+    var title: String?                                              // ??
     
-    func start() {
-        let answer = makeAnswer() // 정답을 만드는 함수
-        //print("[ answer \(answer) ]")
+    func start() -> Int {                                           // 게임시작 함수./ 기능만 만들뿐 아무것도 안하는 상태
+        let answer = makeAnswer() // 정답을 만드는 함수                 // 상수에 정답 만드는 함수 담기 이름()--> 이게 함수표시. 변수상수에 함수 담기 가능
+        print("[ answer \(answer) ]")                               // 답 출력하는 코드.
         
-        print("< 게임을 시작합니다 >")
+        print("< 게임을 시작합니다 >")                                  // 게임시작 문구
+        
+        var tryCount = 0                                            // 시도 횟수
 
-        while true {
+        while true {                                                //while 문: 참일 동안 계속 실행
             // 1. 유저에게 입력값을 받음
             print("숫자를 입력하세요")
-            guard let input = readLine() else { continue }
+            guard let input = readLine() else { continue }              // Q: main.파일에 있는 readLine 이랑 다른것?
             
             // 2. 정수로 변환되지 않는 경우 반복문 처음으로 돌아가기
-            guard let inputNumber = Int(input) else {
+            guard let inputNumber = Int(input) else {                   // ??
                 print("올바르지 않은 입력값입니다 (숫자가 아님)")
                 continue
             }
-
+            let inputNumbers = input.compactMap { Int(String($0)) }     // compactMap??
+            print("inputNumbers \(inputNumbers)")
+            
             // 3. 세자리가 아니거나, 0을 가지거나 특정 숫자가 두번 사용된 경우 반복문 처음으로 돌아가기
-            if input.count != 3 {
+            
+            // 세자리가 아니거나
+            if inputNumbers.count != 3 {
                 print("올바르지 않은 입력값입니다 (자릿수 틀림)")
                 continue
             }
-            else if input.contains("0") {
-                print("올바르지 않은 입력값입니다 (0은 사용못함)")
+            // 0을 가지거나
+            else if inputNumbers[0] == 0 {
+                print("올바르지 않은 입력값입니다 (첫번째 자리에 0 사용못함)")
                 continue
             }
             
-            let s1 = input[input.index(input.startIndex, offsetBy: 0)]
-            let s2 = input[input.index(input.startIndex, offsetBy: 1)]
-            let s3 = input[input.index(input.startIndex, offsetBy: 2)]
-            if s1 == s2 || s2 == s3 || s3 == s1 {
+//            let s1 = input[input.index(input.startIndex, offsetBy: 0)]
+//            let s2 = input[input.index(input.startIndex, offsetBy: 1)]
+//            let s3 = input[input.index(input.startIndex, offsetBy: 2)]
+            
+            //특정 숫자가 두번 사용된 경우
+            if inputNumbers[0] == inputNumbers[1] || inputNumbers[1] == inputNumbers[2] || inputNumbers[2] == inputNumbers[0] {
                 print("올바르지 않은 입력값입니다 (숫자 중복)")
                 continue
             }
 
             // 4. 정답과 유저의 입력값을 비교하여 스트라이크/볼을 출력하기
             // 만약 정답이라면 break 호출하여 반복문 탈출
-            if answer == inputNumber {
-                print("정답입니다!")
-                break
+            var strikeCount = 0
+            var ballCount = 0
+            
+            // 첫번째 숫자가 같을 경우 1스트라이크 증가
+            if answer[0] == inputNumbers[0] {
+                strikeCount += 1
             }
-            else {
-                let answerStr = String(answer)
-                let a1 = answerStr[answerStr.index(answerStr.startIndex, offsetBy: 0)]
-                let a2 = answerStr[answerStr.index(answerStr.startIndex, offsetBy: 1)]
-                let a3 = answerStr[answerStr.index(answerStr.startIndex, offsetBy: 2)]
-                
-                var strikeCount = 0
-                var ballCount = 0
-                
-                if a1 == s1 {
-                    strikeCount += 1
-                }
-                else if s1 == a2 || s1 == a3 {
-                    ballCount += 1
-                }
-                
-                if a2 == s2 {
-                    strikeCount += 1
-                }
-                else if s2 == a1 || s2 == a3 {
-                    ballCount += 1
-                }
-                
-                if a3 == s3 {
-                    strikeCount += 1
-                }
-                else if s3 == a1 || s3 == a2 {
-                    ballCount += 1
-                }
-        //        print("[ strikeCount \(strikeCount) ]")
-        //        print("[ ballCount \(ballCount) ]")
-                
-                if strikeCount == 0 && ballCount == 0 {
-                    print("Nothing")
-                }
-                else if strikeCount != 0 && ballCount != 0 {
-                    print("\(strikeCount)스트라이크 \(ballCount)볼")
-                }
-                else if strikeCount != 0 {
-                    print("\(strikeCount)스트라이크")
-                }
-                else if ballCount != 0 {
-                    print("\(ballCount)볼")
-                }
+            // 입력한 숫자 첫번째가 정답 숫자 2,3번째 숫자와 같을 때 볼 카운트 1 증가
+            else if answer[1] == inputNumbers[0] || answer[2] == inputNumbers[0] {
+                ballCount += 1
+            }
+            
+            // 두번째 숫자가 같을 경우 1스트라이크 증가
+            if answer[1] == inputNumbers[1] {
+                strikeCount += 1
+            }
+            // 입력한 숫자의 두번쨰 숫자가 정답 숫자 2,3 번째랑 같을 떄 볼 카운트 1 증가
+            else if answer[0] == inputNumbers[1] || answer[2] == inputNumbers[1] {
+                ballCount += 1
+            }
+            
+            // 세번째 숫자가 같을 때 1스트라이크 증가
+            if answer[2] == inputNumbers[2] {
+                strikeCount += 1
+            }
+            // 입력한 숫자의 세번째 숫자가 정답 숫자 2,3 번째랑 같을 때 볼 카운트 1 증가
+            else if answer[0] == inputNumbers[2] || answer[1] == inputNumbers[2] {
+                ballCount += 1
+            }
+    //        print("[ strikeCount \(strikeCount) ]")
+    //        print("[ ballCount \(ballCount) ]")
+            
+            //시도 횟수 1씩 증가
+            tryCount += 1                               // tryCount = tryCount + 1
+            
+            //
+            if strikeCount == 3 {                       // 가장 범위가 좁은 확률? 가장 상위에
+                print("정답입니다")
+                return tryCount                         // 정답을 맞추면 시도횟수 같이 보여줌
+            }
+            // 아무숫자도 맞지 않으면 nothing
+            else if strikeCount == 0 && ballCount == 0 {
+                print("Nothing")
+            }
+            // 스트라이크나 볼이 '둘 다' 하나라도 있으면 숫자 출력
+            else if strikeCount != 0 && ballCount != 0 {
+                print("\(strikeCount)스트라이크 \(ballCount)볼")
+            }
+            // 스트라이크가 하나 이상 일 때
+            else if strikeCount != 0 {
+                print("\(strikeCount)스트라이크")
+            }
+            //볼이 하나 이상 일 때
+            else if ballCount != 0 {
+                print("\(ballCount)볼")
             }
         }
     }
     
-    func makeAnswer() -> Int {
-        var n1: Int = 0
-        var n2: Int = 0
-        var n3: Int = 0
-        while true {
-            n1 = Int.random(in: 1...9)
-            n2 = Int.random(in: 1...9)
-            n3 = Int.random(in: 1...9)
-    //        print("\(n1)\(n2)\(n3)")
-
-            if n1 == n2 || n2 == n3 || n3 == n1 {
-                print("wrong answer \(n1)\(n2)\(n3)")
-            }
-            else {
-                break
+    func makeAnswer() -> [Int] {
+        let numbers = (0...9).map { $0 }                    // ??클로저,, map 다시 공부
+        return randomNumbers(numbers, 3)                    // ramdomNumbers 에 인풋으로 numbers 배열 넣고 count 3 리턴
+        
+        // lv1
+//        var n1: Int = 0
+//        var n2: Int = 0
+//        var n3: Int = 0
+//        while true {
+//            n1 = Int.random(in: 1...9)
+//            n2 = Int.random(in: 1...9)
+//            n3 = Int.random(in: 1...9)
+//    //        print("\(n1)\(n2)\(n3)")
+//
+//            if n1 == n2 || n2 == n3 || n3 == n1 {
+//                print("wrong answer \(n1)\(n2)\(n3)")
+//            }
+//            else {
+//                break
+//            }
+//        }
+////        return n1 * 100 + n2 * 10 + n3
+//        return [n1, n2, n3]
+    }
+    
+    func randomNumbers(_ array: [Int], _ count: Int) -> [Int] {
+        guard array.count > count else { return [] }                 // array 배열 갯수가 count(3임) 그니까 3보다 작으면 빈배열 리턴 --?Q: 그럴일이 있나
+        
+        var array = array
+        var elements: [Int] = []
+        while elements.count < count {
+            // pick
+            if let pickElement = array.randomElement(), let pickIndex = array.firstIndex(of: pickElement) {
+    //            print("pickNumber : \(pickElement)")
+    //            print("pickIndex : \(pickIndex)")
+                
+                // if 0 index가 0이면 재추첨. 처음으로 뽑은 숫자가 0이면 재추첨
+                if elements.count == 0 && pickElement == 0 {
+                    print("0인덱스에 0이 나와서 재추첨")
+                    continue
+                }
+                
+                // 뽑기
+                array.remove(at: pickIndex)
+                
+                // 뽑기 결과 리스트에 추가
+                elements.append(pickElement)
             }
         }
-        return n1 * 100 + n2 * 10 + n3
+        return elements
     }
 }
 
--- baseballGame/baseballGame/main.swift
+++ baseballGame/baseballGame/main.swift
@@ -10,5 +10,199 @@ import Foundation
 // main.swift 파일
 // 프로젝트 생성시 자동 생성됨
 
-let game = BaseballGame()
-game.start() // BaseballGame 인스턴스를 만들고 start 함수를 구현하기
+var scoreboard = [Int]()                                                            // [Int]() 이거 뭐야 makes a new empty array-of-Int instance.
+var playing: Bool = true                                                            //q: 와일문에 바로 true 안적고 변수로 받은 이유?
+
+while playing {
+    //게임 시작시 문구
+    print("----------------------------------------------")
+    print("환영합니다! 원하시는 번호를 입력해주세요")
+    print("1. 게임 시작하기  2. 게임 기록보기  3. 종료하기")
+    
+    guard let input = readLine() else { continue }                                  // readLine 이 옵셔널인건 알겠는데 아니면 컨티뉴?? 이해못함 컨티뉴면 다시 와일문 시작?
+    
+    //1. 게임 시작하기 2. 게임 기록보기 3. 종료하기 선택
+    switch input {
+        
+    // 1. 게임 시작하기
+    case "1":
+        let game = BaseballGame()
+        let tryCount = game.start() // BaseballGame 인스턴스를 만들고 start 함수를 구현하기
+        print("tryCount : \(tryCount)")                                             //game.start 가 Int 로 받는건 알겠는데 이게 어케 tryCount 에 숫자로 담기지
+        scoreboard.append(tryCount)                                                 // 게임 시도 할 때마다 시도 획수가 scoreboard 에 배열추가 scorebored는 숫자배열.
+        
+    // 2. 게임 기록보기
+    case "2":
+        print("점수보기")
+        var index = 0
+        for score in scoreboard {                                                   //??? ㅠㅠ scoreboard 배열 만큼
+            index += 1
+            print("\(index)번째 게임 : 시도 횟수 - \(score)")
+        }
+        break
+    
+    // 3. 종료하기
+    case "3":
+        print("게임을 종료합니다")
+        playing = false                                                             // 게임을 종료해야하므로 while 문 false
+        break
+        
+    //1,2,3 중 아무것도 선택하지 않고 다른 숫자나 문자를 입력했을 때
+    default:
+        print("올바른 숫자를 입력해주세요!")
+        break
+    }
+    
+}
+
+
+
+
+//var numbers = (0...9).map { $0 }
+//print("numbers : \(numbers)")
+//let randomIndex = Int.random(in: Range(NSMakeRange(0, numbers.count))!)
+//print("randomIndex : \(randomIndex)")
+//let randomNumber = numbers[randomIndex]
+//print("randomNumber : \(randomNumber)")
+
+
+
+//var list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+//print(list)
+//
+//list = list.map { x in
+//    print(">> \(x)")
+//    return x + 100
+//}
+//print(list)
+//
+//print("---------------클로저----------------")
+//
+//var newList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+//newList = vanmap(newList, closure: { x in
+//    print(">> \(x)")
+//    return x + 100
+//})
+//print(newList)
+//
+//func vanmap(_ array: [Int], closure: (Int) -> Int) -> [Int] {
+//    var resultList: [Int] = []
+//    for x in array {
+//        // x = 99
+//        let userInput = closure(x)
+//        resultList.append(userInput)
+//    }
+//    return resultList
+//}
+//print("-------------------------------")
+
+
+
+
+//
+// closure
+//
+//var age = 19
+//print("-------------------------------")
+//var closure1 = {                                                                  //func c() { print("Hello world") } 함수는 변수에 담을수 있음
+//    print("hello world")
+//}
+//print(closure1)
+//closure1()
+//closure1()
+//closure1()
+//
+//var closure2 = { () -> Int in
+//    print("hello world2")
+//    return 77
+//}
+//print(closure2)                                   // 출력: function
+//print(closure2())                                 // 출력: hello world / 77
+//
+//var closure3 = { (x: String) -> String in
+//    return "hello world3 \(x)"
+//}
+//print(closure3)                                   // 출력: function
+//print(closure3("van"))                            // 출력: hello world3 van
+//
+//print("-------------------------------")
+//
+//func function1() -> Void {                        // Q: 파라미터에는 input 이 담기는 것?
+//    print("hello world (function)")
+//}
+//print(function1)
+//function1()                                       // 출력: hello world (function)
+//
+//func function2() -> Int {
+//    print("hello world2 (function)")
+//    return 177
+//}
+//print(function2)
+//print(function2())
+//
+//func function3(_ x: String) -> String {
+//    "hello world3 \(x)"
+//}
+//print(function3)
+//print(function3("van"))
+//
+//
+//func hjCalcuration(_ n1: Int, _ n2:Int, someOperation: (Int, Int) -> Int) -> Int {        // 기능을 만들기만 한것 아무일도 안일어남
+//    return someOperation(n1, n2)
+//}
+//var result = hjCalcuration(10, 5) { x1, x2 in                                             // 입력 값을 넣고, 변수에 함수를 할당만 한 것. 아무일도 안일어남
+//    x1 + x2 + 1000
+//}
+//print("result = \(result)")                 //변수 result를 호출-> 할당 된 함수 호출-> 함수hj에 입력값 넣어줌
+//print("-------------------------------")
+
+
+
+
+
+//var result = (0...9).map { $0 }
+//print("result : \(result)")
+//result = result.shuffled()
+//print("result : \(result)")
+//
+//var count = 0
+//result = result.filter { x in                         //Q: 해석 못함
+//    if count < 3 && (count == 0 && x != 0) {
+//        count += 1
+//        return true
+//    }
+//    return false
+//}
+//print("result : \(result)")
+
+
+
+//struct Person {
+//    var name: String
+//    var age: Int
+//}
+//
+//let persons = [Person(name: "van", age: 40), nil, Person(name: "hj", age: 26)]
+//let personNames = persons.map { x in x?.name }
+//let personNamesCompact = persons.compactMap { x in x?.name }
+//let personAges = persons.map { x in x?.age }
+//
+////let names: [String] = ["van", "aaaa"]
+////var result = names.map { x in
+////    print(">> \(x)")
+////    return "hey"
+////}
+//
+//print("persons: \(persons)")
+//print("personNames: \(personNames)")
+//print("personNamesCompact: \(personNamesCompact)")
+//print("personAges: \(personAges)")
+//
+//
+//print()
+//print()
+//var personNames2: [String?] = []
+//for person in persons {
+//    personNames2.append(person?.name)
+//}
+//print("personNames2: \(personNames2)")
